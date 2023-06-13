f20a666c76e1064f03adbfeac01cd06e08bf1932
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-14 00:13:48 +0530

    jsx condition return

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 function App() {
-  let jsx = <h1>Hello Universe</h1>;
-  return jsx;
+  let jsx = <h1>Good Morning</h1>;
+  let jsx2 = <h2>Good Night</h2>;
+
+  // let jsx2 = (
+  //   <>
+  //     <h1>Good Night</h1>
+  //   </>
+  // );
+
+  return false ? jsx : jsx2;
 }
 
 export default App;
