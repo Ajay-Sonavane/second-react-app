7c7ebaa2e5efff7e2e1d3c1d36cfb90c40f2d715
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-14 12:55:32 +0530

    id

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ function App() {
   let str = "Hello Universe";
   let pi = 3.144;
   let active = false;
-  let user = { id: 1, Name: "Rohit" };
+  let id = "id1";
 
   return (
     <>
       <h1>Hello World</h1>
       <h1>{str}</h1>
       <h1>pi= {pi}</h1>
-      <h1>
-        {" "}
-        ID= {user.id} Name{user.Name}
-      </h1>
+      <h1 id="id1"> HEllo World</h1>
+      <h1 id={"id1"}> I am AJay</h1>
+      <h1 id={id}> from shirdi</h1>
     </>
   );
 }
