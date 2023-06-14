f2c59384603a5f81af539c8c02bab2a47b29a34e
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-15 00:41:18 +0530

    botostrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 // import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="bg-primary text-light p-3"> My Project </h1>
+
+      <div className="alert alert-primary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        perferendis maiores iste aspernatur cumque sapiente quaerat. Quasi ipsa
+        cupiditate dicta, quibusdam voluptate quis ratione quas amet illo est
+        assumenda necessitatibus!
+      </div>
+      <div className="alert alert-primary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        perferendis maiores iste aspernatur cumque sapiente quaerat. Quasi ipsa
+        cupiditate dicta, quibusdam voluptate quis ratione quas amet illo est
+        assumenda necessitatibus!
+      </div>
+      <div className="alert alert-primary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        perferendis maiores iste aspernatur cumque sapiente quaerat. Quasi ipsa
+        cupiditate dicta, quibusdam voluptate quis ratione quas amet illo est
+        assumenda necessitatibus!
+      </div>
     </>
   );
 }
