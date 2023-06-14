08789a817f57a8d7b9beda26bb100c1eae7b13a9
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-14 11:37:47 +0530

    basic-interpolation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+//Auto-Suggestion and Auto - formating is must be done inorder to know the program properly.
 function App() {
-  let jsx = <h1>Good Morning</h1>;
-  let jsx2 = <h2>Good Night</h2>;
-
-  // let jsx2 = (
-  //   <>
-  //     <h1>Good Night</h1>
-  //   </>
-  // );
-
-  return false ? jsx : jsx2;
+  return (
+    <>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo repellat
+        dolore voluptatem eum eligendi neque quia at sed ut corporis minus
+        veniam blanditiis, consequatur atque vero perferendis dicta natus rerum
+        nemo a impedit! In laboriosam, dolorem voluptatum repellat dolorum
+        cumque? Id, dicta sequi aut nobis eius amet accusamus veritatis! Porro!
+      </h1>
+    </>
+  );
 }
-
 export default App;
