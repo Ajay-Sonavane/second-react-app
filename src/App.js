0645af6be4c4f767a19741f3fe65281487f4ad29
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-14 23:47:10 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+// HTML + JS => JSX
+// Putting the Brain in HTML.
+// Styling :: Color
+// CSS is also JavaScript!!!
 function App() {
-  let str = "Hello Universe";
-  let pi = 3.144;
-  let active = false;
-  let id = "id1";
+  // object literals
+  let style = { background: "green", color: "white" };
 
   return (
     <>
-      <h1>Hello World</h1>
-      <h1>{str}</h1>
-      <h1>pi= {pi}</h1>
-      <h1 id="id1"> HEllo World</h1>
-      <h1 id={"id1"}> I am AJay</h1>
-      <h1 id={id}> from shirdi</h1>
+      <h1 style={style}>Hello World</h1>
+
+      {/** First Curly Brace represents JS Expression */}
+      {/** Second Culry Brace, its object literal */}
+      <h1 style={{}}>Hello</h1>
+      <h1 style={{ background: "blue", color: "white" }}>Hello Again</h1>
+
+      {/** Observations */}
+      {/** Style Attributes become CamelCase in Nature */}
+      {/** Values we are wriing in double quotes */}
+      {/** Multiple props are seperate by comma */}
+
+      <h1 style={{ backgroundColor: "red", color: "white", padding: "8px" }}>
+        hello World
+      </h1>
     </>
   );
 }
+
 export default App;
