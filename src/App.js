d2f3f2d5d0b3f73d6068b072ce9b5b4a5e9f23c7
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-14 11:39:12 +0530

    basic-interpolation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-//Auto-Suggestion and Auto - formating is must be done inorder to know the program properly.
 function App() {
+  //mixing js in the HTML
+  let str = "Hello World";
   return (
     <>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo repellat
-        dolore voluptatem eum eligendi neque quia at sed ut corporis minus
-        veniam blanditiis, consequatur atque vero perferendis dicta natus rerum
-        nemo a impedit! In laboriosam, dolorem voluptatum repellat dolorum
-        cumque? Id, dicta sequi aut nobis eius amet accusamus veritatis! Porro!
-      </h1>
+      <h1> {str}</h1>
+      <h1>{str.toLowerCase()}</h1>
+      <h1>{str.substring(2)}</h1>
     </>
   );
 }
