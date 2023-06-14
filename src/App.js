6ad255af927c7da7c046d38073bd3cf8298043fd
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-15 00:16:58 +0530

    adding bootsrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import "./App.css";
+import "bootstrap/dist/css/bootsrap.css";
 function App() {
-  return <h1 className="beautiful">HEllo Wold</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
 }
-
 export default App;
